b64ffd9d7456ff5c62667d709a0da350bd30d2a6
cstensaa06@gmail.com <cstensaa06@gmail.com>
2021-01-27 10:28:28 -0800

    initial commit

---
+++ rpg fixed/rpg_fixed.py
@@ -0,0 +1,39 @@
+print ('welcome to the realm of mystery')
+name = input('What is your name?\n')
+print ('Hi, %s, Welcome' % name)
+print ('Hurry now we must continue our journey')
+
+Strike = input 
+cut = input
+Stab = input
+goblin = int(20)
+print('Oh my, whats that in the distance? it seems like a goblin patrol, come %s' % name)
+print('theyve seen us, we must strike first. follow me %s' % name)
+print('come now, i suggest we strike, you also know stab and cut' )
+
+
+move1 = vars
+move2 = vars
+move3 = vars
+
+
+move1 = input('what move do you want to use?')
+if move1: stab: goblin = int(14) ('Nice hit, we caught him off guard!')
+if move1: strike: goblin = int(13) ('Nice hit, we caught him off guard!')
+if move1: stab: goblin = int(12) ('Nice hit, we caught him off guard!')
+
+move2 = input('lets do it again!')
+
+if move2: stab: goblin = int(7) ('good job he has to be low!')
+if move2: strike: goblin = int(5) ('good job he has to be low!')
+if move2: cut: goblin = int(6) ('good job he has to be low!')
+
+move3 = input('One More Time!')
+
+if move3: stab: goblin = int(0) ('You Got Him!')
+if move3: strike: goblin = int(0) ('You Got Him!')
+if move3: cut: goblin = int(0) ('You Got Him!')
+
+
+if goblin: int(0)
+print('nice job! see you again soon!')
